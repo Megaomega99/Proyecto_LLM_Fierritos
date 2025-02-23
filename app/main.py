bcd1e7360d0be724f4cf03263463eb032f073dad
Megaomega99 <e.gonzalezg@uniandes.edu.co>
2025-02-23 16:20:52 -0500

    Actualizar main.py para incluir el módulo de usuarios y eliminar archivos __pycache__ obsoletos

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.core.config import settings
-from app.api import auth, documents
+from app.api import auth, documents, users
 from app.db.base import Base
 from app.db.session import engine
 
